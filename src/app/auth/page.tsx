71a9192f4baa123a5db647a654571659810c1719
falanarh <falanarofako29@gmail.com>
2025-07-13 06:10:04 +0700

    Add LoadingState component and implement Suspense in AuthPages for improved loading experience

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,7 @@
 import ModernAuthPage from '@/components/auth/ModernAuthPage';
+import LoadingState from '@/components/other/LoadingState';
 import { Metadata } from 'next';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Login & Register- Survei Digital Wisatawan Nusantara',
@@ -8,8 +10,8 @@ export const metadata: Metadata = {
 
 export default function AuthPages() {
   return (
-    <>
+    <Suspense fallback={<LoadingState />}>
       <ModernAuthPage />
-    </>
+    </Suspense>
   );
 }
